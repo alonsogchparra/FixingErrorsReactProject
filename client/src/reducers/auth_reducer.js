2f24459a9e52d1b80ad8c5ee4df87493808a76ee
Alonsop <alonsop@uakami.com>
2018-05-28 10:25:12 -0400

    new case added on auth_reducer AUTH_ERROR

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_USER, UNAUTH_USER } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
 
 export default(state = {}, action) => {
   switch (action.type) {
@@ -9,6 +9,9 @@ export default(state = {}, action) => {
     case UNAUTH_USER:
       return { ...state, authenticated: false };
       break;
+
+    case AUTH_ERROR:
+      return { ...state, error: action.payload };
   
     default:
       return state;
