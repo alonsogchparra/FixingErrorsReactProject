f8671aab6781b3c7a6c5d790dfbaf3750c260cb7
Alonsop <alonsop@uakami.com>
2018-05-28 10:22:24 -0400

    now will show an error if any info is mistaken

--- client/src/components/auth/signin.js
+++ client/src/components/auth/signin.js
@@ -11,6 +11,16 @@ class Signin extends Component {
     this.props.signinUser({ email, password });
   }
 
+  renderAlert() {
+    if(this.props.errorMessage) {
+      return (
+      <div className="alert alert-danger">
+        <strong>Oops! </strong>{this.props.errorMessage}
+      </div>
+      );
+    }
+  }
+
   render () {
     const { handleSubmit, fields: { email, password }} = this.props;
 
@@ -22,15 +32,20 @@ class Signin extends Component {
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
-          <input {...password} className="form-control" />
+          <input {...password} type="password" className="form-control" />
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign In</button>
       </form>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-},null, actions)(Signin);
+}, mapStateToProps, actions)(Signin);
