13802990e324344e143e3bc9b824923562a8680d
Alonsop <alonsop@uakami.com>
2018-05-28 10:24:20 -0400

    Updating the header with Sign In, Sign Up and Sign Out

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,17 +1,43 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+
+  renderLinks() {
+    if(this.props.authenticated) {
+      // show a link to sign out 
+      return 
+      <li className="nav-item">
+        <Link className="nav-link" to="/signout">Sign Out</Link>
+      </li>
+    } else {
+      // show a link to sign in or sign up
+      return [
+        <li className="nav-item" key={1}>
+          <Link className="nav-link" to="/signin">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link className="nav-link" to="/signup">Sign Up</Link>
+        </li>
+      ];
+    }
+  }
+
   render () {
-    return (
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign In
-          </li>
-        </ul>
-      </nav>
-    );
+      return (
+        <nav className="navbar navbar-light">
+          <Link to="/" className="navbar-brand">Redux Auth</Link>
+          <ul className=" nav navbar-nav">
+            {this.renderLinks()}
+          </ul>
+        </nav>
+      );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
