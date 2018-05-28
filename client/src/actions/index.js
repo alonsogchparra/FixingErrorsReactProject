6db18c7dcf812b3973b536932c5664444e383a7a
Alonsop <alonsop@uakami.com>
2018-05-27 21:32:09 -0400

    updating action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,18 +1,29 @@
 import axios from 'axios';
+import { browserHistory } from "react-router";
+import { AUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
     // Submit email/password to the server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+    .then(response => {
+      // If request is good...
+      // - Update state to indicate user is authenticated
+      dispatch({ type: AUTH_USER });
+      // - Save the JWT token
+      localStorage.setItem('token', response.data.token);
+      // - redirect to the route '/feature'
+      browserHistory.push('/feature');
+    })
+    .catch(() => {
+      // If request is bad...
+      // - Show an error to the User
+    });
 
-    // If request is good...
-    // - Update state to indicate user is authenticated
-    // - Save the JWT token
-    // - redirect to the route '/feature'
 
-    // If request is bad...
-    // - Show an error to the User
+
+    
   }
 }
