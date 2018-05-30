73899a3a0aee356eb460d23c5c9888115012c805
Alonsop <alonsop@uakami.com>
2018-05-30 08:06:15 -0400

    Adding signupUser action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from "react-router";
-import { AUTH_USER, AUTH_ERROR } from './types';
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from './types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -25,9 +25,30 @@ export function signinUser({ email, password }) {
   }
 }
 
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      // Is not working in this way I don't know why, yet.
+      // .catch(response => { dispatch(authError(response.data.error))});
+      .catch(response => dispatch(authError('Email in use')));
+
+  }
+}
+
 export function authError(error) {
   return {
     type: AUTH_ERROR,
     payload: error
   }
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+
+  return { type: UNAUTH_USER };
 }
