9e8d0d5cbfc46041e867b2cc71cabe0e860fe389
Alonsop <alonsop@uakami.com>
2018-05-30 11:14:17 -0400

    fetch message case added

--- client/src/reducers/auth_reducer.js
+++ client/src/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE } from '../actions/types';
 
 export default(state = {}, action) => {
   switch (action.type) {
@@ -13,6 +13,9 @@ export default(state = {}, action) => {
     case AUTH_ERROR:
       return { ...state, error: action.payload };
   
+    case FETCH_MESSAGE:
+      return { ...state, message: action.payload };
+    
     default:
       return state;
       break;
